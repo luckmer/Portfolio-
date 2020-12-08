b006165b7eab668660ee070143beabd41c1cd119
luckmer <woszczo04@gmail.com>
2020-12-08 22:31:17 +0100

    styles

---
+++ src/Styles/Home.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const HeaderStyle = styled.div `
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+export const Span = styled.span `
+  font-size: 10vw;
+  color: #f0f5f9;
+`
+export const Grid = styled.section `
+  padding: 10em 11rem 0 11rem;
+`
+export const ContactPanel = styled.div `
+  width: 100%;
+  padding: 5em 0 0 0;
+  display: flex;
+  h1{
+    padding: 5px 0 5px ;
+    margin: 0 40px 0 40px ;
+  }
+`
---
+++ src/Styles/NavBar.Style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const NavBarContext = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  ul{
+    margin: 20px 30px 10px 30px;
+    list-style-type: none;
+  }
+  li{
+    font-family: gta,sans-serif;
+    font-size: 1.3rem;
+    line-height: 1.7rem;
+    letter-spacing: 1px;
+    font-weight: 100;
+    padding: 0 20px 0 20px ;
+  }
+`
+export const Hr = styled.div `
+  hr {
+    display: block;
+    height: 1px;
+    border-top: 1px solid black;
+  }
+`
+export const Grid = styled.section `
+  font-size: 20px;
+  padding: 4em 11rem 0 11rem;
+  line-height: 1.25;
+`
