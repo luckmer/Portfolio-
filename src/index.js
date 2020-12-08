461cba54eb19fbffbdae3de37cdd764a75e42b20
luckmer <woszczo04@gmail.com>
2020-12-08 22:30:57 +0100

    index

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router } from "react-router-dom";
 import Index from "./Styles/GlobalStyle/GlobalStyle";
 import Store from "./Store/Store";
 import { Provider } from "react-redux";
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={Store}>
         <Index/>
         <App />
     </Provider>
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
