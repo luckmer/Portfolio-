b75c5f9c6fc9b03526779949090a476154a3533b
luckmer <woszczo04@gmail.com>
2020-12-08 22:31:02 +0100

    pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
-
-
+import {Grid, ContactPanel,HeaderStyle,Span} from "../Styles/Home.style";
 
 const Home = () =>{
-
     return(
-        <div>
-
-        </div>
+        <Grid>
+            <HeaderStyle>
+                <Span>Self tough</Span>
+                <Span>programmer & </Span>
+                <Span>Creative developer</Span>
+            </HeaderStyle>
+            <ContactPanel>
+                <h1>LinkedIn</h1>
+                <h1>Github</h1>
+                <h1>Email</h1>
+            </ContactPanel>
+        </Grid>
     )
 }
 
