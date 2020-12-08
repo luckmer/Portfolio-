374380559762f131228a9bebbb37a73f66cf0862
luckmer <woszczo04@gmail.com>
2020-12-08 22:30:43 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import {Home } from "./Export";
+import { Switch, Route, useLocation } from "react-router-dom";
+import {routes } from "./routes";
+import {NavBar} from "./Export";
 
 
 function App() {
-  return (
-    <section>
-      <Home/>
-    </section>
-  );
+    const location = useLocation();
+    return (
+        <section>
+            <NavBar/>
+            <Switch location={location} key={location.pathname}>
+                {routes.map(({ path, Component }) => (
+                    <Route key={path} exact path={path} component={Component} />
+                ))}
+            </Switch>
+        </section>
+    );
 }
 
 export default App;
