f6c97f5d8823a52b70f0ab321c4b454d2fc1c69e
luckmer <woszczo04@gmail.com>
2020-12-13 00:57:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import {routes } from "./routes";
 import {NavBar} from "./Export";
+function App(){
 
-
-function App() {
     const location = useLocation();
     return (
-        <section>
+        <section >
             <NavBar/>
             <Switch location={location} key={location.pathname}>
-                {routes.map(({ path, Component }) => (
-                    <Route key={path} exact path={path} component={Component} />
+                {routes.map(({ path, Component}) => (
+                    <Route key={path} exact path={path} component={Component}  />
                 ))}
             </Switch>
         </section>
@@ -19,3 +18,6 @@ function App() {
 }
 
 export default App;
+
+
+
