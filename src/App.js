552bf779d4b9e062eb15c202b8ca4fd47770437c
luckmer <woszczo04@gmail.com>
2020-12-20 00:09:40 +0100

    📦📦

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import {routes } from "./routes";
 import { AnimatePresence } from 'framer-motion';
-
 import {NavBar} from "./Export";
+
+
+
 function App(){
 
     const location = useLocation();
     return (
         <>
             <NavBar/>
-            <AnimatePresence initial={false} exitBeforeEnter>
+            <AnimatePresence exitBeforeEnter >
                 <Switch location={location} key={location.pathname}>
                     {routes.map(({ path, Component}) => (
                         <Route key={path} exact path={path} component={Component}  />
