00c87d84e3cb8f317118999af52ce89a60fcfc1a
luckmer <woszczo04@gmail.com>
2020-12-22 23:17:08 +0100

    incorrect page fix

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,5 @@ export const routes = [
     { path: "/", Component: Home, name: "home" },
     { path: "/projects", Component: TotalProjects, name: "TotalProjects" },
     { path: "/:_id", Component: ProjectsContextFilter, name: "ProjectsContextFilter" },
-    { path: "*", Component: IncorrectPage, name: "IncorrectPage" },
+    { path: "/*", Component: IncorrectPage, name: "IncorrectPage" },
 ]
