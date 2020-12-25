90c74e897cfeec4c4aab13f3811c93615cc3c86e
luckmer <woszczo04@gmail.com>
2020-12-25 23:36:42 +0100

    .

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
-import {Home,IncorrectPage,ProjectsContextFilter,TotalProjects} from "./Export"
+import {Home,IncorrectPage,ProjectsContextFilter,TotalProjects,Contact} from "./Export"
 
 
 export const routes = [
     { path: "/", Component: Home, name: "home" },
     { path: "/projects", Component: TotalProjects, name: "TotalProjects" },
+    { path: "/Contact", Component: Contact, name: "Contact" },
     { path: "/:_id", Component: ProjectsContextFilter, name: "ProjectsContextFilter" },
     { path: "/*", Component: IncorrectPage, name: "IncorrectPage" },
 ]
