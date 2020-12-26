62f31ca9479d07565a17d32c72e23eb676290000
luckmer <woszczo04@gmail.com>
2020-12-27 00:14:29 +0100

    comment

--- src/hooks/ImageSlide.js
+++ src/hooks/ImageSlide.js
@@ -1,14 +1,14 @@
 const  ImageSlide = (count, Images, setCount) =>  {
 
     const handlePrev = () => {
-        const reset = count === 0;
-        const index = reset ? Images.length - 1 : count - 1;
-        setCount(index);
+        // const reset = count === 0;
+        // const index = reset ? Images.length - 1 : count - 1;
+        // setCount(index);
     };
     const handleNext = () => {
-        const reset = count === Images.length - 1;
-        const index = reset ? 0 : count + 1;
-        setCount(index);
+        // const reset = count === Images.length - 1;
+        // const index = reset ? 0 : count + 1;
+        // setCount(index);
     };
 
     const Mapping = Images.slice(count, count + 1);
