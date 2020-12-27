8d7e5d49ef7ae85b8488c8d1d97b8eef1a672c18
luckmer <woszczo04@gmail.com>
2020-12-27 20:24:12 +0100

    hooks update

--- src/hooks/ScrollAnimateHook.js
+++ src/hooks/ScrollAnimateHook.js
@@ -4,6 +4,9 @@ const  ScrollHook = () => {
     const [state, setState] = useState({
         a: false, b: false, c: false, d :false,main:false
     });
+
+
+    const [ScrollValue ,setScrollValue ]  = useState(document.body.getBoundingClientRect().top)
     const
         e = useRef(null), f = useRef(null),
         g = useRef(null), h = useRef(null);
@@ -12,10 +15,11 @@ const  ScrollHook = () => {
         const topPos = e => e.getBoundingClientRect().top;
 
         const
-            i = Math.abs(topPos(e.current) - 400), j = Math.abs(topPos(f.current) - 400),
-            k = Math.abs(topPos(g.current) - 400), l = Math.abs(topPos(h.current) - 400);
+            i = Math.abs(topPos(e.current) - 300), j = Math.abs(topPos(f.current) ),
+            k = Math.abs(topPos(g.current)), l = Math.abs(topPos(h.current) );
 
         const onScroll = () => {
+            setScrollValue(document.body.getBoundingClientRect().top)
             const a = Math.abs(document.body.getBoundingClientRect().top);
             if (i < a)
                 setState(state => ({ ...state, a: true }));
@@ -32,9 +36,9 @@ const  ScrollHook = () => {
 
         window.addEventListener("scroll", onScroll);
         return () => window.removeEventListener("scroll", onScroll);
-    }, [setState]);
+    }, [setState,setScrollValue]);
 
 
-    return { e, f, g, h ,state};
+    return { e, f, g, h ,state,ScrollValue};
 }
 export default ScrollHook;
