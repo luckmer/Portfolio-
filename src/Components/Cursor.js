1f341ebbf73a507d66c05bfc72cb534abe2662ef
luckmer <woszczo04@gmail.com>
2020-12-29 23:08:39 +0100

    .

--- src/Components/Cursor.js
+++ src/Components/Cursor.js
@@ -4,14 +4,14 @@ import React from "react";
 
 const Cursor = () =>{
     const position = MousePosition();
-
     const cursor = classNames('cursor',{
         'clicked': position.click
     });
 
     return <div className={cursor} style={ {
         left: position.position.x,
-        top:position.position.y}} >
+        top:position.position.y}}
+    >
     </div>
 }
 export default  Cursor
