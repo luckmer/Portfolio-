a28a8aefd240d316d54cf6b86ce6c086ae60699a
luckmer <woszczo04@gmail.com>
2021-01-03 19:58:41 +0100

    update

--- src/hooks/ScrollAnimateHook.js
+++ src/hooks/ScrollAnimateHook.js
@@ -1,6 +1,6 @@
 import {useEffect , useRef,useState} from "react";
 
-const  ScrollHook = () => {
+export const  ScrollHook = () => {
     const [state, setState] = useState({
         a: false, b: false, c: false, d :false,main:false
     });
@@ -41,4 +41,41 @@ const  ScrollHook = () => {
 
     return { e, f, g, h ,state,ScrollValue};
 }
-export default ScrollHook;
+
+
+export const ProjectsContextFilterHook = () =>{
+    const [state, setState] = useState({
+        one: false, two: false, three: false
+    });
+
+    const firstImg = useRef();
+    const secondImg = useRef();
+    const thirdImg = useRef();
+
+    useEffect(() =>{
+        const topPos = e => e.getBoundingClientRect().top;
+        
+        const FirstState = topPos(firstImg.current) - 500;
+        const secondState = topPos(secondImg.current) - 300;
+        
+        const ThirdState = topPos(thirdImg.current)  ;
+        
+        const onScroll = () =>{
+            const scroll = Math.abs(document.body.getBoundingClientRect().top);
+
+            if (FirstState <= scroll) {
+                setState(state => ({...state, one:true}))
+            }
+            if (secondState <= scroll) {
+                setState(state => ({...state, two:true}))
+            }
+            if (ThirdState <= scroll) {
+                setState(state => ({...state, three:true}))
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [setState])
+    
+    return {firstImg,secondImg,thirdImg,state}
+}
