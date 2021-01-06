cf051f60162850f4e6bf82c0a08e0f7518995038
luckmer <woszczo04@gmail.com>
2021-01-06 17:58:47 +0100

    export

--- src/Export.js
+++ src/Export.js
@@ -6,4 +6,6 @@ export {default as AboutMe} from "./Components/AboutMe"
 export {default as ProjectsContextFilter } from "./Pages/ProjectsContextFilter"
 export {default as TotalProjects} from "./Pages/AllProjects";
 export {default as ImageSlide } from "./hooks/ImageSlide";
-export {default as Contact} from "./Pages/Contact";
+export { default as Contact } from "./Pages/Contact";
+export { default as MoreProjectsTemplate } from "./Components/MoreProjectsTemplate";
+export { default as HeaderData } from "./Components/Header";
