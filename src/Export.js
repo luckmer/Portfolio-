3bd2834b8ce0c6cb88574acf662f9084140ef023
luckmer <woszczo04@gmail.com>
2021-01-08 23:28:12 +0100

    export fix

--- src/Export.js
+++ src/Export.js
@@ -1,7 +1,7 @@
 export {default as Home } from "./Pages/Home";
 export {default as IncorrectPage} from "./Pages/404";
 export {default as NavBar} from "./Components/NaBar";
-export {default as Projects } from "./Components/Projects";
+export {default as Projects } from "./Pages/Projects";
 export {default as AboutMe} from "./Components/AboutMe"
 export {default as ProjectsContextFilter } from "./Pages/ProjectsContextFilter"
 export {default as TotalProjects} from "./Pages/AllProjects";
