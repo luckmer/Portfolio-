802fe80b6b6b7b592df3ee7f8ddc3a7b38eda380
luckmer <woszczo04@gmail.com>
2021-01-09 21:15:42 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React,{useEffect} from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import {routes } from "./routes";
 import {NavBar} from "./Export";
-import { ParallaxProvider } from 'react-scroll-parallax';
 function App() {
     const location = useLocation();
 
@@ -11,14 +10,14 @@ function App() {
     }, [location.pathname]);
 
     return (
-        <ParallaxProvider>
+        <main>
             <NavBar />
             <Switch location={location} key={location.pathname}>
                 {routes.map(({ path, Component }) => (
                     <Route key={path} exact path={path} component={Component} />
                 ))}
             </Switch>
-        </ParallaxProvider>
+        </main>
     );
 }
 export default App;
